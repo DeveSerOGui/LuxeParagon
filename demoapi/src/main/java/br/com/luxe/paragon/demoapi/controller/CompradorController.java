e64719609cacd1d804574e7d951d1ad3f29b3bb8
Gui <guifelixs@gmail.com>
2024-04-24 12:31:59 -0300

    Criação do Post, Get ID e PUT no CompradorController.java

--- demoapi/src/main/java/br/com/luxe/paragon/demoapi/controller/CompradorController.java
+++ demoapi/src/main/java/br/com/luxe/paragon/demoapi/controller/CompradorController.java
@@ -26,21 +26,61 @@ class CompradorController {
     @GetMapping
     public ResponseEntity<List<Comprador>> getAll() {
         try {
-
-            Comprador comprador1 = new Comprador();
-            comprador1.setId(1);
-            comprador1.setNome("Guilherme");
-            comprador1.setCpf("123");
-
-            Comprador comprador2 = new Comprador();
-            comprador2.setId(2);
-            comprador2.setNome("João");
-            comprador2.setCpf("456");
-
             return new ResponseEntity<>(Compradores, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+        @PostMapping
+        public ResponseEntity<Comprador> create(@RequestBody Comprador item) {
+            try {
+                Compradores.add(item);
+                return new ResponseEntity<>(item, HttpStatus.CREATED);
+            } catch (Exception e) {
+                return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+            }
+        }
+
+            @GetMapping("{id}")
+            public ResponseEntity<Comprador> getById(@PathVariable("id") Integer id) {
+                Comprador result = null;
+
+                        for(Comprador item : Compradores){
+                            if (item.getId()==id){
+                                result = item;
+                                break;
+                            }
+                        }
+        
+                if (result != null) {
+                    return new ResponseEntity<>(result, HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                }
+            }
+
+
+                @PutMapping("{id}")
+                public ResponseEntity<Comprador> update(@PathVariable("id") Integer id, @RequestBody Comprador CompradorNovosDados) {
+
+                    Comprador compradorAtualizar = null;
+                    
+                    for(Comprador item : Compradores){
+                        if (item.getId()==id){
+                            compradorAtualizar = item;
+                            break;
+                        }
+                    }
+
+                    if (compradorAtualizar == null){
+                        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                    }
+
+                    compradorAtualizar.setNome(compradorAtualizar.getNome());
+                    compradorAtualizar.setCpf(compradorAtualizar.getCpf());
+
+                    return new ResponseEntity<Comprador>(compradorAtualizar, HttpStatus.OK);
+                }
+            
 }
