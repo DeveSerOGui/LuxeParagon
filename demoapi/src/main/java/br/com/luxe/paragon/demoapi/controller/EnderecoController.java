09dffc69aabc248c079b045f8689b6e3d04a7b81
Gui <guifelixs@gmail.com>
2024-05-06 19:14:59 -0300

    Deixando comentado tudo relacionado a endereco

---
+++ demoapi/src/main/java/br/com/luxe/paragon/demoapi/controller/EnderecoController.java
@@ -0,0 +1,11 @@
+//package br.com.luxe.paragon.demoapi.controller;
+//
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+//@RestController
+//@RequestMapping("/comprador/{idUsuario}/endereco")
+//public class EnderecoController {
+//}
--- demoapi/src/main/java/br/com/luxe/paragon/demoapi/model/Endereco.java
+++ demoapi/src/main/java/br/com/luxe/paragon/demoapi/model/Endereco.java
@@ -1,67 +1,67 @@
-package br.com.luxe.paragon.demoapi.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.ManyToOne;
-
-public class Endereco {
-    private Integer id;
-    private String rua;
-
-    private Integer numero;
-
-    private String Cep;
-
-    private Integer Complemento;
-
-    @ManyToOne
-    @JsonIgnore
-    private Comprador Comprador;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getRua() {
-        return rua;
-    }
-
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-
-    public Integer getNumero() {
-        return numero;
-    }
-
-    public void setNumero(Integer numero) {
-        this.numero = numero;
-    }
-
-    public String getCep() {
-        return Cep;
-    }
-
-    public void setCep(String cep) {
-        Cep = cep;
-    }
-
-    public Integer getComplemento() {
-        return Complemento;
-    }
-
-    public void setComplemento(Integer complemento) {
-        Complemento = complemento;
-    }
-
-    public Comprador getComprador() {
-        return Comprador;
-    }
-
-    public void setComprador(Comprador comprador) {
-        Comprador = comprador;
-    }
-}
+//package br.com.luxe.paragon.demoapi.model;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import jakarta.persistence.ManyToOne;
+//
+//public class Endereco {
+//    private Integer id;
+//    private String rua;
+//
+//    private Integer numero;
+//
+//    private String Cep;
+//
+//    private Integer Complemento;
+//
+//    @ManyToOne
+//    @JsonIgnore
+//    private Comprador Comprador;
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getRua() {
+//        return rua;
+//    }
+//
+//    public void setRua(String rua) {
+//        this.rua = rua;
+//    }
+//
+//    public Integer getNumero() {
+//        return numero;
+//    }
+//
+//    public void setNumero(Integer numero) {
+//        this.numero = numero;
+//    }
+//
+//    public String getCep() {
+//        return Cep;
+//    }
+//
+//    public void setCep(String cep) {
+//        Cep = cep;
+//    }
+//
+//    public Integer getComplemento() {
+//        return Complemento;
+//    }
+//
+//    public void setComplemento(Integer complemento) {
+//        Complemento = complemento;
+//    }
+//
+//    public Comprador getComprador() {
+//        return Comprador;
+//    }
+//
+//    public void setComprador(Comprador comprador) {
+//        Comprador = comprador;
+//    }
+//}
