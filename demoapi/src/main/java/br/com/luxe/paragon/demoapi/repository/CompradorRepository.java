5d16190ccd29e3009ffe5e47290506c2cb392874
Gui <guifelixs@gmail.com>
2024-05-09 16:14:41 -0300

    Refactor: Alterando os imports de JPA para Mongo no CompradorRepository.java

--- demoapi/src/main/java/br/com/luxe/paragon/demoapi/repository/CompradorRepository.java
+++ demoapi/src/main/java/br/com/luxe/paragon/demoapi/repository/CompradorRepository.java
@@ -1,9 +1,9 @@
 package br.com.luxe.paragon.demoapi.repository;
 
 import br.com.luxe.paragon.demoapi.model.Comprador;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompradorRepository extends JpaRepository<Comprador, Long> {
+public interface CompradorRepository extends MongoRepository<Comprador, Long> {
 }
